6c70d3d0e9b285ecdf7c637afb1d2435c371c44b
Duwebs <closemailon@gmail.com>
2026-07-10 00:38:05 +0530

    update

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'click-collect-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/dashboard.html',
+  '/style.css',
+  '/script.js'
+];
+
+// Install event: Cache files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Fetch event: Serve from cache if offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
